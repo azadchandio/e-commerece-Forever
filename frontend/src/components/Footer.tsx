7ae57f6d462c37375a6d00e5ae8267b70849dfa6
Azad Chandio <chandiok96@gmail.com>
2025-08-21 09:58:45 +0500

    made footer responsive

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className="relative w-full mb-20 mt-20">
       <div className="w-full px-8 mx-auto max-w-7xl">
         {/* Footer Grid */}
-        <div className="flex flex-col items-center gap-12 md:flex-row md:justify-between">
+        <div className="flex flex-col items-center md:items-start  gap-6 md:flex-row md:justify-between">
           {/* Logo & Description */}
-          <div className="max-w-md">
+          <div className="max-w-md flex flex-col items-center md:items-start">
             <img src={logo} alt="logo" className="w-32" />
             <p className="mt-5 text-sm text-slate-600">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry.
@@ -17,9 +17,9 @@ const Footer = () => {
           </div>
 
           {/* Company Links */}
-          <div>
-            <p className="mb-1 text-base font-semibold text-slate-800">Company</p>
-            <ul>
+          <div >
+            <p className="mb-1  text-base font-semibold text-slate-800 hidden  md:flex ">Company</p>
+            <ul className="flex gap-4 md:flex-col md:gap-0">
               <li>
                 <Link to="/" className="block text-slate-600 py-1 hover:text-slate-500 text-sm">Home</Link>
               </li>
@@ -37,8 +37,8 @@ const Footer = () => {
 
           {/* Contact Info */}
           <div>
-            <p className="mb-1 text-base font-semibold text-slate-800">GET IN TOUCH</p>
-            <ul>
+            <p className="mb-1 text-base font-semibold text-slate-800 hidden  md:flex">GET IN TOUCH</p>
+            <ul className="flex gap-4  justify-center md:flex-col md:gap-0 ">
               <li>
                 <Link to="tel:+12124567890" className="block text-slate-600 py-1 hover:text-slate-500 text-sm">
                   +1-212-456-7890
@@ -54,7 +54,7 @@ const Footer = () => {
         </div>
 
         {/* Footer Bottom */}
-        <div className="flex flex-col items-center justify-center w-full py-4 mt-12 border-t border-slate-200 md:flex-row md:justify-between">
+        <div className="flex flex-col items-center justify-center w-full py-4 mt-8 border-t border-slate-200 md:flex-row md:justify-between">
           <p className="block mb-4 text-sm text-center text-slate-500 md:mb-0">
             © 2025{" "}
             <Link to="/" className="hover:underline">
